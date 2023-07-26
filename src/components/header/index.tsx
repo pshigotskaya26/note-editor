c974073b1288fa10d89ba6df84974e799d406a74
pshigotskaya26 <pshihotskaya.tanya@gmail.com>
2023-07-26 18:25:02 +0300

    feat: style aheader component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <header className="header">
       <div className="container header__container">
-        <h1 className="header__title">Note editor</h1>
+        <h1 className="header__title">
+          <span>Notes</span> manager
+        </h1>
       </div>
     </header>
   );
