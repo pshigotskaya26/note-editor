f8e67a6fa9d79dfa00c1207e0dd07eaecadde3c6
pshigotskaya26 <pshihotskaya.tanya@gmail.com>
2023-07-26 11:13:11 +0300

    feat: install sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import React, { useState } from 'react';
-import { Space, Tag } from 'antd';
+import React from 'react';
 
-const { CheckableTag } = Tag;
-
-const tagsData = ['Movies', 'Books', 'Music', 'Sports'];
-import './App.css';
-
-function App() {
-  const [selectedTags, setSelectedTags] = useState<string[]>(['Books']);
-
-  const handleChange = (tag: string, checked: boolean) => {
-    const nextSelectedTags = checked
-      ? [...selectedTags, tag]
-      : selectedTags.filter((t) => t !== tag);
-    console.log('You are interested in: ', nextSelectedTags);
-    setSelectedTags(nextSelectedTags);
-  };
-  return (
-    <>
-      <span style={{ marginRight: 8 }}>Categories:</span>
-      <Space size={[0, 8]} wrap>
-        {tagsData.map((tag) => (
-          <CheckableTag
-            key={tag}
-            checked={selectedTags.includes(tag)}
-            onChange={(checked) => handleChange(tag, checked)}
-          >
-            {tag}
-          </CheckableTag>
-        ))}
-      </Space>
-    </>
-  );
-}
+const App = () => {
+  return <div>123</div>;
+};
 
 export default App;
