cc7ca88ab9ad556b79e1391bd9cdaaa11e2a9026
pshigotskaya26 <pshihotskaya.tanya@gmail.com>
2023-07-26 19:02:55 +0300

    feat: style a footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import './footer.scss';
 
 const Footer = () => {
-  return <footer className="footer">footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="container footer__container">
+        <p>Created by Pshihotskaya Tatsiana</p>
+        <a
+          className="link footer-link__github"
+          href="https://github.com/pshigotskaya26"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="icon icon_github"></span>
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
