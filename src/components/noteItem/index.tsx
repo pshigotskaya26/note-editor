58bcb191771f0ed48851ba1f57f9f1b0eb44d006
pshigotskaya26 <pshihotskaya.tanya@gmail.com>
2023-07-28 06:58:51 +0300

    fix: fix a noteItem component

--- src/components/noteItem/index.tsx
+++ src/components/noteItem/index.tsx
@@ -9,9 +9,11 @@ const NoteItem = () => {
       <div className="note-item__order">1</div>
       <div className="note-item__content">
         <h3 className="note-item__title">Title 1</h3>
+        <input type="text" className="input-text" value={''} />
         <div className="note-item__description">
           quality and professional print - It doesnt just look high quality, it is high quality.
         </div>
+        <textarea className="textarea" value={''} rows={5}></textarea>
         <div className="note-item__tags">
           <TagsList />
         </div>
@@ -19,7 +21,6 @@ const NoteItem = () => {
       <div className="note-item__controls">
         <div className="note-item__icons">
           <AiFillEdit size={25} className="note-item__edit-icon" />
-
           <AiOutlineCloseCircle size={25} className="note-item__delete-icon" />
         </div>
         <div className="note-item__buttons">
