7becc86dffa9b99de41a1fd0f4a0bd0bec959631
pshigotskaya26 <pshihotskaya.tanya@gmail.com>
2023-07-30 06:56:54 +0300

    feat: create enum for local keys

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,11 +8,9 @@ import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 reportWebVitals();
